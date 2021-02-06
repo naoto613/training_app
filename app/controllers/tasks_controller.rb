3d33bb2c00c369332166fe13616dad9946954b4e
naoto613 <fujii140@gmail.com>
2021-02-06 17:59:22 +0900

    minor-feat: tasksの追加

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      redirect_to new_task_path, alert: '値が不正ですよ'
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      redirect_to edit_task_path, alert: '値が不正ですよ'
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title)
+    end
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  title      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :title, length: { maximum: 20 }
 end
