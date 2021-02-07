83e5760ea5e360a2fa26d04aefc2d40a5e7c2a5b
naoto613 <fujii140@gmail.com>
2021-02-07 23:33:19 +0900

    minor-chore: testです

--- scripts/pr_version_up.sh
+++ scripts/pr_version_up.sh
@@ -4,10 +4,10 @@ set -ex
 
 if [[ $(git diff ".api-version") != "" ]]; then
   UPDATED_VERSION=$(cat ".api-version")
-  BRANCH_NAME="version-up-to-$UPDATED_VERSION-$(date +%s)"
+  BRANCH_NAME="version-up-to-v$UPDATED_VERSION-$(date +%s)"
   git switch -c "$BRANCH_NAME"
   git add ".api-version"
-  COMMIT_MESSAGE="Bump up version to $UPDATED_VERSION"
+  COMMIT_MESSAGE="Bump up version to v$UPDATED_VERSION"
   git commit -m "$COMMIT_MESSAGE"
   git push origin "$BRANCH_NAME"
   URL="https://api.github.com/repos/$OWNER/$REPO/pulls"
